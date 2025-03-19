5d920e74d2b975601377c300ed797c832cfa1641
FBS <aliu2000@outlook.com>
2025-03-19 21:56:42 +0800

    test

--- src/jax_vocos/infer.py
+++ src/jax_vocos/infer.py
@@ -76,9 +76,9 @@ def download_model():
 
     return jax_write_path
 
-def load_model(model_type="44khz",load_path = None):
+def load_model(load_path = None):
     if not load_path:
-        load_path = download_model(model_type)
+        load_path = download_model()
     with open(load_path, "rb") as msgpack_file:
         msgpack_content = msgpack_file.read()
     data = flax.serialization.msgpack_restore(msgpack_content)
